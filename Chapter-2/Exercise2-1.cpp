0b2a9916bca20c7f60493e39ea93f96d8af99c0b
J. Gavin Ray <jgavinray@gmail.com>
2012-01-19 17:48:15 +0000

    Exercise 2.1 #6 Page 56

---
+++ Chapter-2/Exercise2-1.cpp
@@ -0,0 +1,18 @@
+// File: Exercise2-1.cpp
+// Description: Exercise 2.1 # 6 from Page 56
+// By: J. Gavin Ray
+// Date: 1/19/2012
+
+#include <iostream>
+using namespace std;
+
+main()
+{
+    char StopProgram;       // Variable to Stop the Program Nothing Else
+    cout << "J. Gavin Ray\n";
+    cout << "200 Burnett Ave #123\n";
+    cout << "Morgan Hill, Ca 95037\n";
+    cin  >> StopProgram;
+
+    return 0;
+}
