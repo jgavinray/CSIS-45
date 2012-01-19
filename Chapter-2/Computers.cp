f2a44230191bef47c1835a5dd0850a21d084ddec
J. Gavin Ray <jgavinray@gmail.com>
2012-01-19 09:05:48 -0800

    New Stuffs

---
+++ Chapter-2/Computers.cp
@@ -0,0 +1,17 @@
+// File: Computers.cpp
+// Description:  Basic Output Program
+// Gavin Ray
+// Date: 1/19/2012
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Computers, computers everywhere\n";
+    cout << "as far as I can C\n\n";
+    cout << "- J. Gavin Ray\n";
+    
+    return 0;
+
+}
---
+++ Chapter-2/MyName.cpp
@@ -0,0 +1,20 @@
+// File: MyName.cpp
+// Description:  Displaying My Name and Address
+// Gavin Ray
+// Date: 1/19/2012
+
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout << "Brought to you by:\n\n";
+    cout << "J. Gavin Ray\n";
+    cout << "200 Burnett Ave #123\n";
+    cout << "Morgan Hill, Ca\n";
+    cout << "95037\n\n";
+    cout << "Sponsered by: Gavilan College\n\n";
+    
+    return 0;
+    
+}
