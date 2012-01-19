4676cae71dd41d6751e8e9fc38298e152039fd6a
J. Gavin Ray <jgavinray@gmail.com>
2012-01-19 08:48:12 -0800

    Hello World Program

---
+++ Chapter-2-HelloWorld/hello.cpp
@@ -0,0 +1,14 @@
+// File: Hello.cpp
+// Description: Displays the Classic Hellow World!
+// Gavin Ray
+// Date 1/19/2012
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    
+    return 0;
+}
