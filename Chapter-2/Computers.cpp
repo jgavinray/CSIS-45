1ac8ff816e490585b09846f0db8d17aa294f2e6f
J. Gavin Ray <jgavinray@gmail.com>
2012-01-19 17:41:24 +0000

    Added Computers.cpp

---
+++ Chapter-2/Computers.cpp
@@ -0,0 +1,22 @@
+// File: Computers.cpp
+// Description:  Basic Output Program
+// Gavin Ray
+// Date: 1/19/2012
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char x;     // Used to to stop outputs nothing more.
+    cout << "Computers, computers everywhere\n";
+    cout << "as far as I can C\n\n";
+    cout << "And now all I C is C++ (CPP)!\n\n";
+    cout << "Into the near future I C Java, followed closely by Ruby!\n\n";
+    cout << "And into the far future I barely see Rails...\n\n";
+    cout << "- J. Gavin Ray\n";
+    cin  >> x;  // Used to stop outputs nothing more.
+
+    return 0;
+
+}
