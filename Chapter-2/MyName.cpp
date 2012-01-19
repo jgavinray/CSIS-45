c5af67c877d285c96d6e392dea0435548b9efef9
J. Gavin Ray <jgavinray@gmail.com>
2012-01-19 17:39:05 +0000

    Assignments with Screen Shots

--- Chapter-2/MyName.cpp
+++ Chapter-2/MyName.cpp
@@ -8,13 +8,15 @@ using namespace std;
 
 main()
 {
+    char x;     // Variable to allow cin to stop program.
+
     cout << "Brought to you by:\n\n";
     cout << "J. Gavin Ray\n";
     cout << "200 Burnett Ave #123\n";
     cout << "Morgan Hill, Ca\n";
     cout << "95037\n\n";
     cout << "Sponsered by: Gavilan College\n\n";
-    getch();
+    cin  >> x;  // Used just to stop output for screen shot.
 
     return 0;
 
